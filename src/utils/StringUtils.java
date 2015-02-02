a3b4c0e846d0ab2ecabed24150a1f04f90ccd32c
christophe-laptop <antlantisgate@gmail.com>
2015-02-02 09:30:47 +0100

    Changes in StringUtils class

--- src/utils/StringUtils.java
+++ src/utils/StringUtils.java
@@ -1,7 +1,17 @@
 package utils;
 
+/**
+ * Class containing utility functions on Strings
+ */
+
 public abstract class StringUtils
 {
+    /**
+     * compare two strings
+     * @param a string 1
+     * @param b string 2
+     * @return true if equal
+     */
 	public static boolean equal(String a, String b)
 	{
 		return a == b || (a != null && a.equals(b));
